9babb8b2176b4be9f965d8df75ead3ebd27a215c
ElKaloria <maks.dektyarev@yandex.ru>
2024-05-28 12:27:45 +0700

    small fixes

--- MyShop/MyShop/settings.py
+++ MyShop/MyShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e=ru^p#6lrsudq2r6^u@l4+t&xso3a-h-&f3@o81ntghry$$ld'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +77,15 @@ WSGI_APPLICATION = 'MyShop.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ShopDB',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres123',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
@@ -117,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+THUMBNAIL_PREFIX = 'products/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
